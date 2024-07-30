c7296f34f356760b071415a0ae16de5e4678ae49
Nytrix-09 <75318042+Nytrix-09@users.noreply.github.com>
2024-07-30 18:30:22 -0400

    first react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
+  
+  //creating a state hook
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+  
+  
+  //addItem
+  function addItem() {
+
+    if(!newItem){
+      alert("Enter a todo!");
+      return;
+    }
+
+    //add item to the list
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem
+    };
+
+    setItems(oldList => [...oldList, item]);
+
+    setNewItem("");
+  }
+
+  
+  //deleteItem
+  function deleteItem (id) {
+    //delete item from the list
+    const newArray = items.filter(item => item.id !== id);
+    setItems(newArray);
+    
+    };
+  
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>toDoList App</h1>
+   
+   
+    <input type ="text" 
+      placeholder='Add a to-do!' 
+      value={newItem}
+      onChange={e => setNewItem(e.target.value)}/>  
+    
+    
+    <button onClick={() =>addItem()}>Add</button>
+
+    <ul>
+      {items.map(item => {
+        return (
+          <li key={item.id}>{item.value} <button onClick={() => deleteItem(item.id)}>Delete</button></li>
+          )
+      })}   
+    </ul>
+  </div>
   );
 }
 
